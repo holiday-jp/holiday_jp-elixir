cd18323ebf6b3fc011bc07bf3c3d9e28868d8a93
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-02-19 21:40:26 +0900

    v0.3.6

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule HolidayJp.Mixfile do
       ],
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
-      version: "0.3.5",
+      version: "0.3.6",
 
       # Docs
       docs: [
